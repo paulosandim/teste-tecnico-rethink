dae17cc11218bd11096962b04f303a2f05fd0a46
Paulo Sandim <pauloedusandim@gmail.com>
2025-06-25 21:10:46 -0300

    tentativa de desacoplamento na exclusão

---
+++ desacoplamento/exclusao-usuario.test.js
@@ -0,0 +1,26 @@
+const request = require('supertest')
+const { api, setupUsuario } = require('../utils/testSetup')
+
+describe('Exclusão de usuário', () => {
+  let jwt, usuario
+
+  beforeAll(async () => {
+    usuario = await setupUsuario()
+    await request(api).get(`/confirm-email?token=${usuario.confirmToken}`)
+    const resLogin = await request(api).post('/login').send({
+      email: usuario.email,
+      password: usuario.password
+    })
+    jwt = resLogin.body.token
+  })
+
+  it('exclui a conta do usuário', async () => {
+    const res = await request(api)
+      .delete('/account')
+      .set('Authorization', `Bearer ${jwt}`)
+      .send({ password: usuario.password })
+  
+    expect(res.status).toBe(200)
+    expect(res.body).toHaveProperty('message')
+  })
+})
